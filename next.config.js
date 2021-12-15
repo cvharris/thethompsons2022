f655553ccffd456df4a95c0bce2a02efd9ec3da1
Charlie Harris <charris1014@gmail.com>
2021-12-15 01:34:31 -0600

    Attempts to fix loading svgs

--- next.config.js
+++ next.config.js
@@ -7,6 +7,10 @@ module.exports = {
     path: 'https://res.cloudinary.com/dy6xelvys/image/upload',
   },
   webpack(config) {
+    const fileLoaderRule = config.module.rules.find(
+      (rule) => rule.test && rule.test.test('.svg')
+    )
+    fileLoaderRule.exclude = /\.svg$/
     config.module.rules.push({
       test: /\.svg$/,
       issuer: {and: [/\.(js|ts)x?$/]},
